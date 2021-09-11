4edb0cb92aff526e1f381bf5999caab47dfa8d7b
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-11 13:01:58 -0300

    Init

---
+++ app/src/main/java/com/study/kotlin/whoiam/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.study.kotlin.whoiam.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.study.kotlin.whoiam.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
