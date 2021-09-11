d1461eff087b374f3e5d7528ca851194fa54e6aa
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-11 16:05:12 -0300

    Create: Activity - signo_screen (view)

---
+++ app/src/main/java/com/study/kotlin/whoiam/signo_screen.java
@@ -0,0 +1,14 @@
+package com.study.kotlin.whoiam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signo_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signo_screen);
+    }
+}
