57fa7774be5b5333a7f7c53a2d18f077536fa7a3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-12 15:26:56 -0300

    Binding: Activity - Main (view.ui.main)

--- app/src/main/java/com/study/kotlin/whoiam/view/ui/main/MainActivity.java
+++ app/src/main/java/com/study/kotlin/whoiam/view/ui/main/MainActivity.java
@@ -3,14 +3,20 @@ package com.study.kotlin.whoiam.view.ui.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.study.kotlin.whoiam.R;
+import com.study.kotlin.whoiam.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(binding.inflate());
+        binding = ActivityMainBinding.inflate(getLayoutInflater())
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
