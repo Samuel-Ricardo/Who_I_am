c41424e08393f49d5ca7f41b06d013965c1f05ff
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-12 17:00:18 -0300

    Create: Class - Signo (domain.model)

---
+++ app/src/main/java/com/study/kotlin/whoiam/domain/model/Signo.java
@@ -0,0 +1,28 @@
+package com.study.kotlin.whoiam.domain.model;
+
+import org.jetbrains.annotations.Contract;
+
+public class Signo {
+
+    /*
+    public enum Signos {
+
+        ARIES(new Signo());
+
+        private Signo signo;
+
+        Signos(Signo signo) {
+
+            signo = signo;
+        }
+
+        @Contract(pure = true)
+        public Signo getSigno() {
+            return signo;
+        }
+    }
+    */
+
+    public
+}
+
